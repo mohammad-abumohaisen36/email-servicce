39e24865ca54bf0a2a0bd2650b05a417c327d8a4
mohammadabumohaisen <m.abumohaisen@mt-jor.com>
2024-02-15 13:40:18 +0300

    init commit

---
+++ src/main/java/com/app/emailservicce/EmailServicceApplication.java
@@ -0,0 +1,13 @@
+package com.app.emailservicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServicceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServicceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/emailservicce/EmailServicceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.emailservicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServicceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
